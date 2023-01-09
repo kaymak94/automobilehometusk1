30e010a11aa08de5f5be170efc3417c259a399b7
Каймакчи Александр <kaymakchiao@mail.ru>
2023-01-09 20:57:31 +0300

    Initial commit

---
+++ Desktop/Puppeeter2/jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  launch: {
+    slowMo: 400,
+    headless: false,
+    defaultViewport: null,
+    args: ["--start-maximized"], //— используем максимальный размер окна браузера
+  },
+};
---
+++ Desktop/Puppeeter2/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  verbose: true,
+  preset: "jest-puppeteer",
+  testTimeout: 70000,
+};
